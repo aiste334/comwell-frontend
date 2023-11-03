e974099683df97770c2e02ee9eec9d27e58908a7
Glória <63670594+VingardiumGloriosa@users.noreply.github.com>
2023-11-03 23:25:21 +0100

    ServiceSection
    what is missing
    1) figuring out if its acutally optimized or i was sleep deprived
    2) image rendering is being an arse

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,19 +1,24 @@
 import Header from '@/src/components/header/Header'
 import '@/src/styles/globals.css'
 import offersStuff from "@/public/dummy-backend/offers.json"
+import ServiceSection from '../components/services/ServiceSection'
 
 export default function App({ Component, pageProps }) {
 
-  const renderOffers = () => {
+  /*const renderOffers = () => {
     return offersStuff.map(offerThing => {
       return <OfferCard
               info={offerThing}
             />
     })
   }
+  
+  this  belongs int the return
+      {renderOffers()}*/
 
   return (<>
     <Header name="Comgood"></Header>
-    {renderOffers()}
+      <ServiceSection>
+      </ServiceSection>
   </>)
 }
