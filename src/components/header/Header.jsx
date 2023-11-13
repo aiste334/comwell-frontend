903dba0a253c4619a59b7e58ffeabf2d93f81b19
Glória <63670594+VingardiumGloriosa@users.noreply.github.com>
2023-11-13 21:22:32 +0100

    UGHH

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -3,10 +3,8 @@ import Image from 'next/image';
 import Link from 'next/link';
 import logoSvg from '@/public/logos/logo.svg';
 import dropdownIcon from '@/public/icons/dropdown.svg';
-import profileIcon from '@/public/icons/profile.svg';
 import menuIcon from '@/public/icons/menu.svg';
 import HeaderButton from './HeaderButton';
-import LoginPopup from './LoginPopup';
 
 function Header() {
   const [isLoginPopupOpen, setLoginPopupOpen] = useState(false);
@@ -22,11 +20,9 @@ function Header() {
       </Link>
       <div className="flex gap-6">
         <HeaderButton onClick={() => {}}>Lokationer <Image src={dropdownIcon} /></HeaderButton>
-        <HeaderButton onClick={toggleLoginPopup}>Profil <Image src={profileIcon} /></HeaderButton>
         <HeaderButton onClick={() => {}}>Menu <Image src={menuIcon} /></HeaderButton>
       </div>
 
-      <LoginPopup isOpen={isLoginPopupOpen} onClose={toggleLoginPopup} />
     </div>
   );
 }
