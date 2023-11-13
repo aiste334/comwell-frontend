ac5f48ab86d7f1379620324eafaf6afb0838d00e
Glória <63670594+VingardiumGloriosa@users.noreply.github.com>
2023-11-13 21:14:04 +0100

    attempt num 2

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,9 +1,7 @@
 import Header from '@/src/components/header/Header'
 import '@/src/styles/globals.css'
 import offersStuff from "@/public/dummy-backend/offers.json"
-import ServiceSection from '../components/services/ServiceSection'
 import Footer from '../components/footer/Footer'
-import LoginPopup from '../components/header/LoginPopup'
 
 export default function App({ Component, pageProps }) {
 
@@ -18,14 +16,12 @@ export default function App({ Component, pageProps }) {
   this  belongs int the return
       {renderOffers()}
            
-      <Footer></Footer>
 
       */
 
   return (<>
     <Header name="Comgood"></Header>
-      
-    <ServiceSection>
-      </ServiceSection>
+    <Footer></Footer>
+
   </>)
 }
