fb72b896e22a38ec6fa6938eab2ce62c6e983a1e
aiste334 <aistekst344@gmail.com>
2023-12-20 15:01:00 +0100

    auth update small

--- src/pages/api/auth/[...nextauth].js
+++ src/pages/api/auth/[...nextauth].js
@@ -16,6 +16,7 @@ export default NextAuth({
         try {
           const response = await fetch('http://localhost:4000/auth/login', {
             method: 'POST',
+            credentials: 'include',
             headers: { 'Content-Type': 'application/json' },
             body: JSON.stringify({ email: username, password}),
           });
